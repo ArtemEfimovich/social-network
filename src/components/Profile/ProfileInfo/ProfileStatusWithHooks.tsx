d94dbfdc4da2c6a99f4930a4bfc8b8dfb9e5af10
ArtemEfimovich <artemfender@gmail.com>
2021-09-25 20:37:58 +0300

    useEffect

--- src/components/Profile/ProfileInfo/ProfileStatusWithHooks.tsx
+++ src/components/Profile/ProfileInfo/ProfileStatusWithHooks.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 
 type ProfileStatusType = {
     status: string;
@@ -10,6 +10,11 @@ const ProfileStatusWithHooks = (props: ProfileStatusType) => {
 
     let [editMode, setEditMode] = useState(false)
     let [status, setStatus] = useState(props.status)
+
+    useEffect(()=>{
+        setStatus(props.status)
+    },[props.status])
+
 
 
     const activateEditMode = () => setEditMode(true)
@@ -25,7 +30,6 @@ const ProfileStatusWithHooks = (props: ProfileStatusType) => {
     }
 
 
-   /* if(prevProps.status !== this.props.status)*/
 
     return (
         <div>
